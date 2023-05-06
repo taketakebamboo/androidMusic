f89ec4889b72ff92616694480700f76ff369923b
taketakebamboo <takeuguisu@gmail.com>
2023-05-06 22:41:11 +0900

    [add] StertButtonとStopButtonを押したときの処理を記述

--- app/src/main/java/com/example/androidmusic/MainActivity.kt
+++ app/src/main/java/com/example/androidmusic/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
+    private val musicManager: MusicManager = MusicManager(this@MainActivity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,8 +14,18 @@ class MainActivity : AppCompatActivity() {
         //stertButton
         val strbtn: Button = findViewById(R.id.startbutton)
 
+        // stertButtonを押したときの処理を記述
+        strbtn.setOnClickListener {
+            musicManager.stertMusic()
+        }
+
         //stopButton
         val stpbtn: Button = findViewById(R.id.stopbutton)
+
+        // stopButtonを押したときの処理を記述
+        stpbtn.setOnClickListener {
+            musicManager.stopMusic()
+        }
 
         // loopSwitch
         val loopswt: Switch = findViewById(R.id.loopswitch)
