aed4f072cbc71e7e4da08199aec8e39313e94abe
taketakebamboo <takeuguisu@gmail.com>
2023-05-06 22:20:04 +0900

    [add] MusicManagerクラスに音楽を挿入、再生するstertMusicメソッドとstopMusicメソッドを実装。

--- app/src/main/java/com/example/androidmusic/MusicManager.kt
+++ app/src/main/java/com/example/androidmusic/MusicManager.kt
@@ -1,4 +1,27 @@
 package com.example.androidmusic
 
-class MusicManager {
+import android.content.Context
+import android.media.MediaPlayer
+
+class MusicManager(context: Context) {
+    private lateinit var mediaPlayer: MediaPlayer
+    private val myContext: Context = context
+
+    fun stertMusic() {
+        // MediaPlayer型のmediaPlayer変数にMediaPlayerクラスのcreateメソッドで音楽を挿入
+        mediaPlayer = MediaPlayer.create(myContext,R.raw.Debussy_clair_de_lune)
+        // mediaPlayer型のmediaPlayer変数のisLooping変数にtrueを代入
+        mediaPlayer.isLooping = true
+        // mediaPlayer変数のstertメソッド呼び出し
+        mediaPlayer.start()
+    }
+
+    fun stopMusic() {
+        // mediaPlayer変数のstopメソッド呼び出し
+        mediaPlayer.stop()
+        // mediaPlayer変数のresetメソッド呼び出し
+        mediaPlayer.reset()
+        // mediaPlayer変数のreleaseメソッド呼び出し
+        mediaPlayer.release()
+    }
 }
