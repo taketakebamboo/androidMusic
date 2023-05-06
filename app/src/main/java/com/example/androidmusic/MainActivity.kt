28fae502b2c77b575d0aab11a82a107acfab39ea
taketakebamboo <takeuguisu@gmail.com>
2023-05-06 22:06:45 +0900

    [add] 音楽ファイルと各種ボタン、スイッチの変数を設定。新たにMusicManagerクラスファイルを設置。

--- app/src/main/java/com/example/androidmusic/MainActivity.kt
+++ app/src/main/java/com/example/androidmusic/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.androidmusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //stertButton
+        val strbtn: Button = findViewById(R.id.startbutton)
+
+        //stopButton
+        val stpbtn: Button = findViewById(R.id.stopbutton)
+
+        // loopSwitch
+        val loopswt: Switch = findViewById(R.id.loopswitch)
+
+
     }
 }
