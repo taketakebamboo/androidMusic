d9f6c17a30f48153749a542f8c1fdd418e5ed4e2
taketakebamboo <takeuguisu@gmail.com>
2023-05-07 18:43:32 +0900

    [update] startbuttonとstopbuttonから再生ボタンに変更に伴い、MainActivityの処理内容を変更

--- app/src/main/java/com/example/androidmusic/MainActivity.kt
+++ app/src/main/java/com/example/androidmusic/MainActivity.kt
@@ -4,39 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
+import com.example.androidmusic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val musicManager: MusicManager = MusicManager(this@MainActivity)
     private var startFlag: Boolean = false
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //startButton
-        val strbtn: Button = findViewById(R.id.startbutton)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        // startButtonを押したときの処理を記述
-        strbtn.setOnClickListener {
-            if(!startFlag){
-                musicManager.startMusic()
-                startFlag = true
-            }
-
-        }
-
-        //stopButton
-        val stpbtn: Button = findViewById(R.id.stopbutton)
-
-        // stopButtonを押したときの処理を記述
-        stpbtn.setOnClickListener {
-            if(startFlag){
-                musicManager.stopMusic()
-                startFlag = false
+        binding.playImage.setOnClickListener {
+            when(startFlag) {
+                false -> {
+                    musicManager.startMusic()
+                    startFlag = true
+                    binding.playImage.setImageResource(R.drawable.playbutton_300x300)
+                }
+                true -> {
+                    musicManager.stopMusic()
+                    startFlag = false
+                    binding.playImage.setImageResource(R.drawable.posebutton_300x300)
+                }
             }
         }
 
         // loopSwitch
-        val loopswt: Switch = findViewById(R.id.loopswitch)
+        // val loopswt: Switch = findViewById(R.id.loopswitch)
 
     }
 
